eb00d3d7b791ad2b37ad586c80093445e6b7dc99
Mahesh <moholkarmahesh@aca.edu.in>
2023-08-13 21:22:09 +0530

    Added More Controllers, Added Registory Pattern

---
+++ Dto/PokemonDto.cs
@@ -0,0 +1,11 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Dto
+{
+    public class PokemonDto
+    {
+        public int PokemonId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
